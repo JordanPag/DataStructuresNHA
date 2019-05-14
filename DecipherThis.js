c77ecd6990fd858e8e8d1b0bc431b8fcbe592b3c
JordanPag <JordanPag@users.noreply.github.com>
2019-05-13 21:07:46 -0400

    The rest of HW1

--- DecipherThis.js
+++ DecipherThis.js
@@ -2,8 +2,8 @@ function decipherThis(str) {
   str = str.split(" ");
   let decoded = "";
   for(let i=0; i<str.length;i++) {
-    let unicode = str[i].replace(/\D/g, "");
-    let word = str[i].replace(/[0-9]/g, '');
+    let unicode = str[i].replace(/\D/g, ""); //Reduce to only numbers
+    let word = str[i].replace(/[0-9]/g, ''); //Remove all numbers
     let newWord;
     if(word.length!=1) {
       newWord = String.fromCharCode(unicode) + word.charAt(word.length-1) + word.substring(1,word.length-1) + word.charAt(0);
